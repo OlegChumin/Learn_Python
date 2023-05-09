3255de0896864694587f6f79f4a7732b3f0b35ba
Oleg <tschumin.oleg@gmail.com>
2023-05-09 16:18:48 +0300

    Add new files

--- lessons/lesson_018_list.py
+++ lessons/lesson_018_list.py
@@ -1,2 +1,15 @@
 number_list = [32, 21, 48, 34.56]
 print(number_list)
+
+some_list=[12, 35.334, 'hello']
+print(len(some_list))
+print(some_list)
+
+print(some_list[1])
+another_list = some_list[:2]
+print(another_list)
+print(some_list)
+some_list[2] = 'hi'
+print(some_list)
+new_list = some_list + another_list
+print(new_list)
