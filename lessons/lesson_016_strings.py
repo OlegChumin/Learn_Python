abf3f6fd86fd872b40ee5c8469761fcdedaa0dfa
Oleg <tschumin.oleg@gmail.com>
2023-05-09 12:55:50 +0300

    Add new files

--- lessons/lesson_016_strings.py
+++ lessons/lesson_016_strings.py
@@ -1,4 +1,5 @@
 #string methods and properties
+#string are immutable
 first_name = "Jake"
 print(first_name[2]) #k
 #first_name[2] = 'n' #TypeError: 'str' object does not support item assignment
@@ -11,9 +12,12 @@ print(last_letter)
 new_first_name = first_two_letters + 'n' + last_letter
 print(new_first_name)
 
+#concatination
 greeting = 'Hello'
 print(greeting)
 greeting = greeting +" Python"
 print(greeting)
 greeting += "!"
 print(greeting)
+
+#
