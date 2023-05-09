8c11ae25d506d224336b530e699331372daf741e
Oleg <tschumin.oleg@gmail.com>
2023-05-09 09:21:02 +0300

    Add new files

---
+++ lessons/lesson_014_strings.py
@@ -0,0 +1,7 @@
+greeting = "Hello"
+first_name = "Jack"
+last_name = 'White'
+exclamation_symbol = "!"
+white_space = " "
+print(greeting + first_name + last_name)
+print(greeting + " " + first_name + white_space + last_name + exclamation_symbol)
