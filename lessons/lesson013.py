f860896fdcf6cb71550b4d29c93f26e0c0c95a15
Oleg <tschumin.oleg@gmail.com>
2023-05-09 09:13:44 +0300

    Add new files

---
+++ lessons/lesson013.py
@@ -0,0 +1,14 @@
+credit = 2_200_000
+credit_rate = 22.8
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+print(final_sum) #Банк ВТБ идет в ж...
+
+car_model = "FORD"
+car_color = "black"
+car_year = 1975
+car_door_number = 5
+print(car_model, car_color, car_year, car_door_number) #При использовании оператора запятой , в функции print()
+# в Python между выводимыми значениями добавляется пробел по умолчанию.
+car_color = "white"
+print(car_model, car_color, car_year, car_door_number)
