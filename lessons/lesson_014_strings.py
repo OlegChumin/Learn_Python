ac3bc845f666b5e245c27c25f1393a96469fab16
Oleg <tschumin.oleg@gmail.com>
2023-05-09 09:43:27 +0300

    Add new files

--- lessons/lesson_014_strings.py
+++ lessons/lesson_014_strings.py
@@ -1,3 +1,5 @@
+import time
+
 greeting = "Hello"
 first_name = "Jack"
 last_name = 'White'
@@ -5,3 +7,11 @@ exclamation_symbol = "!"
 white_space = " "
 print(greeting + first_name + last_name)
 print(greeting + " " + first_name + white_space + last_name + exclamation_symbol)
+long_string = "This is long string"
+print(long_string)
+
+print("Hello!", end="", flush=True)
+time.sleep(1)
+print("\rWorld!", end="", flush=True)
+print()
+
