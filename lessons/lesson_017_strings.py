4080da90c721cf50b21f310a382c7bad46bff127
Oleg <tschumin.oleg@gmail.com>
2023-05-09 16:12:33 +0300

    Add new files

---
+++ lessons/lesson_017_strings.py
@@ -0,0 +1,10 @@
+print(1 + 1)
+print('1' + '1')
+age = 47
+print('Jack is ' + str(age) + ' years old.')
+
+age = 23
+name = 'Jack'
+name_and_age = 'My name is {0}. I\'m {1} years old'.format(name, age)
+print(name_and_age)
+
---
+++ lessons/lesson_018_list.py
@@ -0,0 +1,2 @@
+number_list = [32, 21, 48, 34.56]
+print(number_list)
