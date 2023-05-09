5e1bd44d8717aed8d38c71c2147d39e42dd81edc
Oleg <tschumin.oleg@gmail.com>
2023-05-09 09:58:24 +0300

    Add new files

--- lessons/lesson_014_strings.py
+++ lessons/lesson_014_strings.py
@@ -9,9 +9,18 @@ print(greeting + first_name + last_name)
 print(greeting + " " + first_name + white_space + last_name + exclamation_symbol)
 long_string = "This is long string"
 print(long_string)
+whole_sentence = greeting + white_space + first_name + " " \
+    + last_name + exclamation_symbol
+print(whole_sentence)
 
 print("Hello!", end="", flush=True)
 time.sleep(1)
 print("\rWorld!", end="", flush=True)
 print()
+
+#Escaping
+some_string = "I'm a programmer"
+another_string = 'I want to lear "Python"'
+new_string = 'I\'m a programmer'
+another_new_string = "I want to lear \"Python\""
 
