cadb820812c429458ce53ccb5f1791cc5439b75e
Oleg <tschumin.oleg@gmail.com>
2023-05-09 12:11:35 +0300

    Add new files

--- lessons/lesson_016_strings.py
+++ lessons/lesson_016_strings.py
@@ -2,4 +2,12 @@
 first_name = "Jake"
 print(first_name[2]) #k
 #first_name[2] = 'n' #TypeError: 'str' object does not support item assignment
-print(first_name)
+#print(first_name)
+
+first_two_letters = first_name[:2]
+print(first_two_letters) #Ja
+last_letter = first_name[-1]
+print(last_letter)
+new_first_name = first_two_letters + 'n' + last_letter
+print(new_first_name)
+
