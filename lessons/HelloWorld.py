93693c35672a5b343987a18e9b7b97a5da54c89b
Oleg <tschumin.oleg@gmail.com>
2023-05-09 08:35:33 +0300

    Add new files

--- lessons/HelloWorld.py
+++ lessons/HelloWorld.py
@@ -2,3 +2,24 @@ print("Hello Python!")
 print('Hello Python!!!')
 print('He said, "Hello, World!"')
 print('I will be a Python developer!')
+print()
+print("Сложение 2 + 2 = ", 2 + 2)
+print("Вычитание 2 - 2 = ", 2 - 2)
+print("Умножение 2 * 2 = ", 2 * 2)
+print("Деление 2 / 2 = ", 2 / 2) #Результат деления в Python 3 дает float а в Python 2 будет int
+print("Остаток от деления 5 на 2 = ", 5 % 2) #modulus operator - jостаток от деления
+print(5 // 2) #такое деление работает одинаково в обоих версиях результат int
+print("Возведение 2 в степень 3 = ", 2 ** 3) #возведение в степень
+print()
+
+"""
+Это многострочный комментарий.
+Он может содержать несколько строк.
+"""
+
+my_string = 'Hello, world!'
+if 'H' in my_string:
+    print('The string contains the character "H"')
+else:
+    print('The string does not contain the character "H"')
+
